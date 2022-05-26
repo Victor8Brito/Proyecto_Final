e21f87ae4620d056ce756c22d5ba9d7bca5df9d9
Victor8Brito <victor8brito@gmail.com>
2022-05-26 15:47:39 -0500

    git commit -m "Actualizado"

--- app/src/main/java/com/example/proyectofinal/RecyclerView.kt
+++ app/src/main/java/com/example/proyectofinal/RecyclerView.kt
@@ -2,10 +2,44 @@ package com.example.proyectofinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.proyectofinal.rickandmorty.AdaptadorPersonaje
+import com.example.proyectofinal.rickandmorty.Personaje
 
 class RecyclerView : AppCompatActivity() {
+    lateinit var miRecyclerView: RecyclerView
+    val listPersonajes = ArrayList<Personaje>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+        miRecyclerView = findViewById(R.id.RecyclerPersonajes)
+        miRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+        getPersonajes()
+    }
+
+    fun getPersonajes(){
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://rickandmortyapi.com/api/character"
+        val jsonObject = JsonObjectRequest(
+            Request.Method.GET,url,null,
+            { respuesta->
+                val newjson =respuesta.getJSONArray("results")
+                for (i in 0..newjson.length()-1) {
+                    val newobj = newjson.getJSONObject(i)
+                    val personaje = Personaje(newobj.getString("name"),
+                        newobj.getString("image"))
+                    listPersonajes.add(personaje)
+                }
+                miRecyclerView.adapter = AdaptadorPersonaje(listPersonajes)
+            },{ error->
+                Log.e("Exam","Error")
+            }
+        )
+        queue.add(jsonObject)
     }
 }
---
+++ app/src/main/java/com/example/proyectofinal/rickandmorty/Personaje.kt
@@ -0,0 +1,3 @@
+package com.example.proyectofinal.rickandmorty
+
+data class Personaje(val nombre: String, val imagen: String)
