3b028b9fb744c399ecab41e45531fd4948679fe7
Victor8Brito <victor8brito@gmail.com>
2022-05-25 23:53:17 -0500

    git commit -m "Actualizado"

--- app/src/main/java/com/example/proyectofinal/TablasDeMultiplicar.kt
+++ app/src/main/java/com/example/proyectofinal/TablasDeMultiplicar.kt
@@ -1,11 +1,20 @@
 package com.example.proyectofinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.proyectofinal.tablas.Practica
 
 class TablasDeMultiplicar : AppCompatActivity() {
+    lateinit var btnPracticar:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tablas_de_multiplicar)
+        btnPracticar= findViewById(R.id.btnIrAPracticar)
+        btnPracticar.setOnClickListener{
+            val intentPracticar= Intent(this, Practica::class.java)
+            startActivity(intentPracticar)
+        }
     }
 }
---
+++ app/src/main/java/com/example/proyectofinal/tablas/ResultadoTablas.kt
@@ -0,0 +1,33 @@
+package com.example.proyectofinal.tablas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
+import com.example.proyectofinal.R
+
+class ResultadoTablas : AppCompatActivity() {
+    lateinit var  animView: LottieAnimationView
+    lateinit var  btnRegresarAMultiplicacion: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado_tablas)
+
+        btnRegresarAMultiplicacion= findViewById(R.id.btnRegresar)
+        btnRegresarAMultiplicacion.setOnClickListener{
+            val intentMultiplicacion= Intent(this,Practica::class.java)
+            startActivity(intentMultiplicacion)
+        }
+
+        animView = findViewById(R.id.animation_view)
+
+        val extras = intent.extras
+        val respuestaCorrecta = extras!!.getBoolean("respuesta")
+        if (respuestaCorrecta){
+            animView.setAnimation(R.raw.success)
+        }else{
+            animView.setAnimation(R.raw.wrongnotif)
+        }
+    }
+}
